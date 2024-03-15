d12a903c2b2b9dfcb11843b1949a1fba5ce84701
thaibhoang <hoangthai.ftu53@gmail.com>
2024-03-15 09:33:48 +0700

    Setup Flight and Airport associations, add id to foreign_key

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,5 @@
 class Airport < ApplicationRecord
-  has_many :departing_flights, foreign_key: "deprature_airport", class_name: "Flight"
-  has_many :arriving_flights, foreign_key: "arrival_airport", class_name: "Flight"
+  has_many :departing_flights, foreign_key: "deprature_airport_id", class_name: "Flight"
+  has_many :arriving_flights, foreign_key: "arrival_airport_id", class_name: "Flight"
 
 end
