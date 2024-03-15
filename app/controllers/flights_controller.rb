9f20510527d7e5ea8f39c806a5d8aaff07b1f60f
thaibhoang <hoangthai.ftu53@gmail.com>
2024-03-15 22:21:26 +0700

    Add search result to view underneath search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,10 +3,13 @@ class FlightsController < ApplicationController
     @flight = Flight.new
     @airport_options = Airport.all.order(:code).map { |a| [a.code, a.id]}
     @date_options = Flight.where('start_at >= ?', Time.now).pluck(Arel.sql('DISTINCT DATE(start_at) AS date')).sort().map { |date| [date, date] }
-    @flight_search_results = nil
-
+    @number_of_seats = 1
+    
     if params[:flight]
-      @flight_search_results = Flight.where(flight_query_params).where("DATE(start_at) = ?", params[:flight_date] )
+      @flight_search_results = Flight.where(flight_query_params).where("DATE(start_at) = ?", params[:flight_date] ).order(:start_at).includes([:departure_airport, :arrival_airport])
+      @flight = Flight.new(flight_query_params)
+      @number_of_seats = params[:number_of_seats]
+      @flight_date = params[:flight_date]
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,21 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-%w(ATL PEK DXB LAX ORD LHR HND HKG PVG CDG AMS DFW CAN FRA IST DEL CGK SIN ICN DEN BKK JFK KUL SFO MAD CTU LAS BCN BOM YYZ SEA CLT LGW SZX TPE MEX KMG MUC MCO MIA PHX SYD EWR MNL SHA XIY FCO IAH NRT SVO CKG DMK MSP GRU BOS SGN DOH HGH DTW JED MEL FLL ORL SAW BOG DME CJU LGA PHL DUB ZRH CPH KIX PMI MAN OSL LIS ARN BWI AYT STN NKG GMP BLR RUH BRU DUS XMN VIE CGO SLC YVR DCA CSX AUH CUN FUK TAO BNE WUH).each do |airport_code|
+%w(ATL PEK DXB LAX ORD LHR HND HKG PVG CDG AMS DFW).each do |airport_code|
   Airport.find_or_create_by(code: airport_code)
 end
-# Create 100 random flights
-# 100.times do
-#   two_airports = Airport.order("RANDOM()").limit(2)
-#   departure, arrival = two_airports
-#   random_seconds = rand(15*24*60) * 60
-#   go_back_seven_days = 7*24*60*60
-#   random_start = Time.now - go_back_seven_days + random_seconds
-#   random_duration = rand(60..360) 
-#   Flight.create(
-#     start_at: random_start,
-#     duration: random_duration,
-#     departure_airport_id: departure.id,
-#     arrival_airport_id: arrival.id
-#   )
-# end
+# Create 1000 random flights
+1000.times do
+  two_airports = Airport.order("RANDOM()").limit(2)
+  departure, arrival = two_airports
+  random_seconds = rand(15*24*60) * 60
+  go_back_seven_days = 7*24*60*60
+  random_start = Time.now - go_back_seven_days + random_seconds
+  random_duration = rand(60..360) 
+  Flight.create(
+    start_at: random_start,
+    duration: random_duration,
+    departure_airport_id: departure.id,
+    arrival_airport_id: arrival.id
+  )
+end
