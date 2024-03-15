60c89aab10c7b2b81a9c88dc60c21ee7882b1621
thaibhoang <hoangthai.ftu53@gmail.com>
2024-03-15 09:13:45 +0700

    setup seed airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+%w(ATL PEK DXB LAX ORD LHR HND HKG PVG CDG AMS DFW CAN FRA IST DEL CGK SIN ICN DEN BKK JFK KUL SFO MAD CTU LAS BCN BOM YYZ SEA CLT LGW SZX TPE MEX KMG MUC MCO MIA PHX SYD EWR MNL SHA XIY FCO IAH NRT SVO CKG DMK MSP GRU BOS SGN DOH HGH DTW JED MEL FLL ORL SAW BOG DME CJU LGA PHL DUB ZRH CPH KIX PMI MAN OSL LIS ARN BWI AYT STN NKG GMP BLR RUH BRU DUS XMN VIE CGO SLC YVR DCA CSX AUH CUN FUK TAO BNE WUH).each do |airport_code|
+  Airport.find_or_create_by(code: airport_code)
+end
