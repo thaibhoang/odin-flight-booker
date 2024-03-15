5be134e666b46a5cab627508f6300449b86abdd0
thaibhoang <hoangthai.ftu53@gmail.com>
2024-03-15 09:32:16 +0700

    Setup Flight and Airport associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,5 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, foreign_key: "deprature_airport", class_name: "Flight"
+  has_many :arriving_flights, foreign_key: "arrival_airport", class_name: "Flight"
+
 end
