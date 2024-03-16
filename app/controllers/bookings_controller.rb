d6f5867971fba266d21aa791544652636b355037
thaibhoang <hoangthai.ftu53@gmail.com>
2024-03-16 15:49:45 +0700

    Send data from search flight to booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,21 @@
 class BookingsController < ApplicationController
   def new
+    if !params[:booking]
+      @booking = Booking.new
+      @passengers_array = [Passenger.new]
+    else
+      @booking = Booking.new(after_search_booking_params)
+      @flight = Flight.find(params[:booking][:flight_id])
+      @passengers_array = []
+      for i in 1..params[:number_of_passengers].to_i do
+        @passengers_array << Passenger.new
+      end
+    end
+  end
 
+  private
+
+  def after_search_booking_params
+    params.require(:booking).permit(:flight_id)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :passenger
   belongs_to :flight
   belongs_to :user
+
+  accepts_nested_attributes_for :passenger
 end
